904b047be021ac83d46f50511865e12490ff07ac
gilad <gsharir@picscout.com>
2014-12-21 14:41:33 +0200

    accuracy layer outputs class confusion in top blob

---
+++ src/caffe/layers/accuracy_layer.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <vector>
+
+#include "caffe/layer.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void AccuracyLayer<Dtype>::LayerSetUp(
+  const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
+  top_k_ = this->layer_param_.accuracy_param().top_k();
+}
+
+template <typename Dtype>
+void AccuracyLayer<Dtype>::Reshape(
+  const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
+  CHECK_EQ(bottom[0]->num(), bottom[1]->num())
+      << "The data and label should have the same number.";
+  CHECK_LE(top_k_, bottom[0]->count() / bottom[0]->num())
+      << "top_k must be less than or equal to the number of classes.";
+  CHECK_EQ(bottom[1]->channels(), 1);
+  CHECK_EQ(bottom[1]->height(), 1);
+  CHECK_EQ(bottom[1]->width(), 1);
+  n_labels = bottom[0]->count() / bottom[0]->num();
+  (*top)[0]->Reshape(n_labels, n_labels, 1, 1);
+  //(*top)[0]->Reshape(1, 1, 1, 1);
+}
+
+template <typename Dtype>
+void AccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    vector<Blob<Dtype>*>* top) {
+  Dtype accuracy = 0;
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  const Dtype* bottom_label = bottom[1]->cpu_data();
+  int num = bottom[0]->num();
+  int dim = bottom[0]->count() / bottom[0]->num();
+  vector<Dtype> maxval(top_k_+1);
+  vector<int> max_id(top_k_+1);
+
+  //CHECK_EQ(n_labels,(int)*std::max_element(bottom_label, bottom_label+num)+1)
+  //	  << "number of labels must be equal to ..";
+  vector<Dtype> class_accuracy(n_labels,0);
+  //vector<Dtype> num_per_class(n_labels,0);
+  vector<vector<Dtype> > class_confusion(n_labels,class_accuracy);
+
+  for (int i = 0; i < num; ++i) {
+    // Top-k accuracy
+    std::vector<std::pair<Dtype, int> > bottom_data_vector;
+    for (int j = 0; j < dim; ++j) {
+      bottom_data_vector.push_back(
+          std::make_pair(bottom_data[i * dim + j], j));
+    }
+    std::partial_sort(
+        bottom_data_vector.begin(), bottom_data_vector.begin() + top_k_,
+        bottom_data_vector.end(), std::greater<std::pair<Dtype, int> >());
+    // check if true label is in top k predictions
+    for (int k = 0; k < top_k_; k++) {
+    	class_confusion[static_cast<int>(bottom_label[i])][bottom_data_vector[k].second]++;
+    /*
+      if (bottom_data_vector[k].second == static_cast<int>(bottom_label[i])) {
+    	class_accuracy[static_cast<int>(bottom_label[i])]++;
+        ++accuracy;
+        break;
+      }
+      */
+
+    }
+    //num_per_class[static_cast<int>(bottom_label[i])]++;
+  }
+
+
+  LOG(INFO) << "# labels: " << n_labels;
+  Dtype* vec = (*top)[0]->mutable_cpu_data();
+  for (int i=0; i < n_labels; ++i){
+
+	  for (int j=0; j < n_labels; ++j)
+	  {
+
+		  const int idx = i*n_labels + j;
+	  //LOG(INFO) << num_per_class[i];
+		  vec[idx] = class_confusion[i][j];
+	  }
+
+	  //vec += vec->offset( );
+
+  }
+
+  // Accuracy layer should not be used as a loss function.
+}
+
+INSTANTIATE_CLASS(AccuracyLayer);
+
+}  // namespace caffe
